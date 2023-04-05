4b05a02c79c3b86ec7f209ccb389a7e74217a945
Noel Palviainen <ad0815@student.jamk.fi>
2023-04-05 14:29:11 +0300

    Eagle now flies in a circle

--- Assets/Scripts/EagleCircle.cs
+++ Assets/Scripts/EagleCircle.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class EagleCircle : MonoBehaviour
 {
-    public float speed;
-    public float rotation =4;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject eagle;
+    [SerializeField] GameObject empty;
 
-    void Update()
-    {
-        transform.Rotate(Vector3.left * rotation * Time.deltaTime);
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
-  
-    }
+    // Positive numbers make the eagle fly clockwise and negative numbers make it move counter-clockwise
+    [SerializeField] float direction = -1;
+    //You can change the speed of the eagle from the inspector.
+    [SerializeField] float flySpeed = 60;
+    void Update() =>
+       transform.RotateAround(empty.transform.position,
+           new Vector3(0, direction, 0), flySpeed * Time.deltaTime);
+
+    //Source: https://stackoverflow.com/questions/38724961/how-to-make-a-gameobject-rotate-around-another-gameobject-in-unity-in-the-rotati
 }
