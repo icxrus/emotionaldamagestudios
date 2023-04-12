f83f1f9dc80f495f70778782fe93b6910423c3dc
Eeli <ad0260@student.jamk.fi>
2023-04-12 13:11:16 +0300

    Changes to scripts
    Fixed shop related functionality

--- Assets/SFInventory/Scripts/TestItemsAdder.cs
+++ Assets/SFInventory/Scripts/TestItemsAdder.cs
@@ -2,33 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SFInventory;
+using UnityEngine.UI;
 
 namespace SFInventory.Demo
 {
     public class TestItemsAdder : MonoBehaviour
     {
+        //THIS SCRIPT MUST REMAIN INSIDE INVENTORY PREFAB
+        // TestItems[x] x=>0
         public SFInventoryItem[] testItems;
-        public int addCount = 5;
+        public int addCount;
+        public int shopItemTag;
         private SFInventoryManager inventoryManager;
+
+        //Price handeling
+        // Text displaying player´s money
+        public Text _playerMoney;
+        // Boolena fro testing if player has enough money to do a purchase
+        [SerializeField] private bool _CanBuy;
+        // Int saving the ammount player has.
+        public int _playerOwnedMoney;
+        public int _itemPrice;
 
         void Start()
         {
             inventoryManager = GetComponent<SFInventoryManager>();
         }
-
-        void Update()
+        void CheckMoney()
         {
-            //here all the numbers of the button that are pressed will be converted to an integer and with this number you will add the item to your inventory
-            if (Input.anyKey)
+            if(_playerOwnedMoney >= _itemPrice)
             {
-                if (int.TryParse(Input.inputString, out int i) && i <= testItems?.Length && i > 0)
-                {
-                    inventoryManager.AddItemsCount(testItems[i - 1], addCount, out int left);
-                    //if there is not enough space in your inventory, you will get the remaining number of items back
-                    if (left > 0)
-                        Debug.Log("Inventory overflow: " + left + " " + testItems[i - 1].Name);
-                }
+                _CanBuy = true;
             }
+            else { _CanBuy = false; }
+        }
+
+        public void AddItemToInventory()
+        {
+            CheckMoney();
+            if (_CanBuy == true) 
+            {
+                // Coonects to inventory manager to add the item.
+                inventoryManager.AddItemsCount(testItems[shopItemTag], addCount, out int left);
+                //if there is not enough space in your inventory, you will get the remaining number of items back
+                if (left > 0) { Debug.Log("Inventory overflow: " + left + " " + testItems[shopItemTag].Name); }
+
+                // Remove money here
+                _playerOwnedMoney -= _itemPrice;
+                // Update Sprites
+                _playerMoney.text = "Shards: " + _playerOwnedMoney.ToString();
+            }
+            else { Debug.Log("Not Enough Money"); }
+
         }
     }
 }
--- Assets/Scripts/Shopping/NewShopManager.cs
+++ Assets/Scripts/Shopping/NewShopManager.cs
@@ -6,51 +6,5 @@ using SFInventory;
 
 public class NewShopManager : MonoBehaviour
 {
-    // Text displaying player´s money
-    public Text _playerMoney;
-    // Boolena fro testing if player has enough money to do a purchase
-    [SerializeField] private bool _hasMoney;
-    // Int saving the ammount player has.
-    public int _playerOwnedMoney;
-
-    // Method used to by item
-    public void PurcahaseItem()
-    {
-        if (_hasMoney == true)
-        {
-
-        }
-        //Move to inventory
-        //Reduce money
-        //Save inventory automatically
-        
-        //Update sprites
-        _playerMoney.text = "Shards: " + _playerOwnedMoney.ToString();
-    }
-
-    // Method used to chek if player has enough money
-    public void CheckMoney()
-    {
-        //Get the money from save
-        //Get cost of item from store
-        //Compare ammount
-        //Returnn boolean
-    }
-    public SFInventoryItem[] testItems;
-    public int addCount = 1;
-    private SFInventoryManager inventoryManager;
-    public int _itemPointer;
-    void Start()
-    {
-        inventoryManager = GetComponent<SFInventoryManager>();
-    }
-
-    //here all the numbers of the button that are pressed will be converted to an integer and with this number you will add the item to your inventory
-    void AddItemToInventory()
-    {
-        //[UNCOMMENT]inventoryManager.AddItemsCount(_itemPointer, addCount, out int left);
-        //if there is not enough space in your inventory, you will get the remaining number of items back
-        //if (left > 0)
-            Debug.Log("Not Enough Inventory Space");
-    }
+    
 }
