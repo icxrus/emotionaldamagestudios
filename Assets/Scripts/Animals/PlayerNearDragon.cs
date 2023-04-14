738cf147f7f5db9248349e5bd1c6753dd1e38ef3
Noel Palviainen <ad0815@student.jamk.fi>
2023-04-14 15:02:42 +0300

    Dragon now flies higher

--- Assets/Scripts/Animals/PlayerNearDragon.cs
+++ Assets/Scripts/Animals/PlayerNearDragon.cs
@@ -5,28 +5,44 @@ using UnityEngine;
 public class PlayerNearDragon : MonoBehaviour
 {
     [SerializeField] private Animator playerNearD;
-    [SerializeField] private float heightSpeed= 4;
+
+    [SerializeField] private GameObject target;
     [SerializeField] private GameObject dragon;
+
     [SerializeField] private bool PlayerNear;
+    [SerializeField] private float speed = -500;
+    [SerializeField] private float timer = 4f;
 
-
-
+    //Player enters trigger sphere and makes animation happen.
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerNearD.SetBool("PlayerNear", true);
+            PlayerNear = true;
         }
-        PlayerNear = true;
     }
+
+    public void Update()
+    {
+        if (PlayerNear == true)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                dragon.transform.position = Vector3.MoveTowards(dragon.transform.position, target.transform.position, speed * Time.deltaTime);
+            }
+        }
+        
+    }
+
+    // Makes the bool values for PlayerNear false, but does not do anything.
     public void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerNearD.SetBool("PlayerNear", false);
+            PlayerNear = false;
         }
-        PlayerNear = false;
     }
-    float whatever;
-    float secondY;
 }
